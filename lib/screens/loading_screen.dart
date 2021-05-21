ac1725f43ef881aa16c86d8a7f626faea4096934
Garima Bisht <bisht.garima01@gmail.com>
2021-05-21 14:46:23 +0530

    first commit

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+void getLocation() async {
+  try {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+    print(position);
+  }
+  catch (e) {
+    print(e);
+  }
+}
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState(){
+    super.initState();
+  }
+//  print('init is called');
+
+  Widget build(BuildContext context) {
+    print('build is called');
+     return Scaffold(
+     );
+    //   body: Center(
+    //     child: TextButton(
+    //       onPressed: () {
+    //         getLocation();
+    //       },
+    //       child: Text('Get Location'),
+    //     ),
+    //   ),
+  }
+}
