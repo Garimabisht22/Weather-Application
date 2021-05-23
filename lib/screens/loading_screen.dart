4e140f567a661c2487811c4bd34e0a10df74d134
Garima Bisht <bisht.garima01@gmail.com>
2021-05-23 12:48:20 +0530

    Added http request. Now able to fetch data from the internet.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,40 +1,64 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void getLocation() async {
-  try {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low);
-    print(position);
-  }
-  catch (e) {
-    print(e);
-  }
-}
-
+const kApiKey = '953144532f238f23099574e828b7e991';
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
   @override
-  void initState(){
+
+  void initState() {
     super.initState();
+    getLocation();
   }
-//  print('init is called');
+
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    longitude= location.longitude;
+    latitude = location.latitude;
+    getData();
+  }
+
+  void getData() async {
+    var url = Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$kApiKey');
+    var response = await http.post(url, body: {
+    });
+    print(response.statusCode );
+    //  Response response = await get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22';
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var temp = jsonDecode(data)['main']['temp'];
+      print(temp);
+
+      var id = jsonDecode(data)['weather'][0]['id'];
+      print(id);
+
+      var city = jsonDecode(data)['name'];
+      print(city);
+
+    } else{
+      print(response.statusCode);
+    }
+  }
 
   Widget build(BuildContext context) {
-    print('build is called');
-     return Scaffold(
-     );
-    //   body: Center(
-    //     child: TextButton(
-    //       onPressed: () {
-    //         getLocation();
-    //       },
-    //       child: Text('Get Location'),
-    //     ),
-    //   ),
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+          },
+          child: Text('Get Location'),
+        ),
+      ),
+    );
   }
 }
