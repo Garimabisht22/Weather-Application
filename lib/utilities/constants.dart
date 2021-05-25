d2d253aee6da5db85de13892600b5d5378dd453c
Garima Bisht <bisht.garima01@gmail.com>
2021-05-26 00:04:58 +0530

    Added new constant values

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,20 +1,43 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+const kApiKey = '953144532f238f23099574e828b7e991';
+const kApiMapUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
 const kTempTextStyle = TextStyle(
-  fontFamily: 'Spartan MB',
-  fontSize: 100.0,
+  fontFamily: 'Newsreader',
+  fontSize: 120.0,
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
 );
 
 const kMessageTextStyle = TextStyle(
-  fontFamily: 'Spartan MB',
-  fontSize: 60.0,
+  fontFamily: 'Pacifico',
+  fontSize: 50.0,
+  color: Colors.black,
 );
-
 const kButtonTextStyle = TextStyle(
   fontSize: 30.0,
-  fontFamily: 'Spartan MB',
+  fontFamily: 'Newsreader',
+  color: Colors.white,
 );
 
-const kConditionTextStyle = TextStyle(
-  fontSize: 100.0,
+const kConditionTextStyle = TextStyle(fontSize: 140.0, color: Colors.white);
+const kTextFieldInputDecoration = InputDecoration(
+    filled: true,
+    fillColor: Colors.white,
+    icon: Icon(
+      Icons.location_city,
+      color: Colors.white,
+      size: 45,
+    ),
+    hintText: 'Enter City Name',
+    hintStyle: TextStyle(
+        color: Colors.grey
+    ),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(10),
+      ),
+      borderSide: BorderSide.none,
+    )
 );
